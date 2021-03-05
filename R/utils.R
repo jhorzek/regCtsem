be2eccfe06b73f15de06a8a87f1c56b9371fc91f
Jannik <orzek@mpib-berlin.mpg.de>
2021-03-05 11:11:55 +0100

    added function to check non-convexity

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,126 @@
+#' checkNonConvexity
+#'
+#' checks the non-convexity of a provided model within the provided parameter bounds numerically.
+#'
+#' This is a very simple implementation of the concept described in Tamura, K., & Gallagher, M. (2019). Quantitative measure of nonconvexity for black-box continuous functions. Information Sciences, 476, 64–82. https://doi.org/10.1016/j.ins.2018.10.009
+#' The basic idea is as follows: The function generates nSample points within the provided parameter bounds. For each combination of these sample points the following condition is evaluated:
+#' f(x1) + f(x2) >= 2*f((x1 + x2)/2)  (midpoint convexity, see Equation 2 in Tamura, K., & Gallagher, M., 2019)
+#' This condition should evaluate to TRUE for all possible combinations of x1 and x2. If it evaluates to FALSE, this implies that the model fitting function is not convex.
+#' WARNING: Even if all evaluations indicate that midpoint convexity is given, this is no proof that the function is convex! However, if a counter-example is found (i.e., at least one evaluation results in FALSE), this implies
+#' that the function might be nonconvex. Keep in mind that this is only a numerical check and susceptible to numerical errors
+#' @param model model of type ctsemFit, Rcpp_cpptsemRAMmodel, Rcpp_cpptsemKalmanModel, or MxRAMModel
+#' @param lowerBound vector of the same length as the number of parameters in the model specifying the lower bound for each parameter
+#' @param upperBound vector of the same length as the number of parameters in the model specifying the upper bound for each parameter
+#' @param nSample number of sample points between the lower and upper bound
+#' @return returns the generated samplePoints, a logical vector indicating if the midpoint convexity was given for the comparison of these sample points (isConvex) and the difference f(x1) + f(x2) - 2*f((x1 + x2)/2) for each combination of sample points. This difference should be positive for all comparisons!
+#' @return
+checkNonConvexity <- function(model, lowerBound = NULL, upperBound = NULL, nSample = 50){
+  if(any(class(model) == "ctsemFit")){
+    model <- model$mxobj
+    pars <- omxGetParameters(model)
+    getFit <- function(mxObject, pars){
+      mxObject <- omxSetParameters(mxObject, labels = names(pars), values = pars)
+      mxObject <- mxRun(mxObject, useOptimizer = FALSE, silent = TRUE)
+      return(mxObject$fitfunction$result[[1]])
+    }
+  }else if(any(class(model) == "Rcpp_cpptsemRAMmodel")){
+    pars <- model$getParameterValues()
+    getFit <- function(Rcpp_cpptsemRAMmodel, pars){
+      Rcpp_cpptsemRAMmodel$setParameterValues(pars, names(pars))
+      o <- try(Rcpp_cpptsemRAMmodel$computeRAM())
+      if(any(class(o) == "try-error")){return(NA)}
+      o <- try(Rcpp_cpptsemRAMmodel$fitRAM())
+      if(any(class(o) == "try-error")){return(NA)}
+      return(Rcpp_cpptsemRAMmodel$m2LL)
+    }
+  }else if(any(class(model) == "Rcpp_cpptsemKalmanModel")){
+    pars <- model$getParameterValues()
+    getFit <- function(Rcpp_cpptsemKalmanModel, pars){
+      Rcpp_cpptsemKalmanModel$setParameterValues(pars, names(pars))
+      o <- try(Rcpp_cpptsemKalmanModel$computeAndFitKalman())
+      if(any(class(o) == "try-error")){return(NA)}
+      return(Rcpp_cpptsemKalmanModel$m2LL)
+    }
+  }else if(any(class(model) == "MxRAMModel")){
+    pars <- omxGetParameters(model)
+    getFit <- function(mxObject, pars){
+      mxObject <- omxSetParameters(mxObject, labels = names(pars), values = pars)
+      mxObject <- mxRun(mxObject, useOptimizer = FALSE, silent = TRUE)
+      return(mxObject$fitfunction$result[[1]])
+    }
+  }else{stop("model of unknown class")}
+
+  if(is.null(lowerBound)){lowerBound <- pars - 3*abs(pars)}
+  if(is.null(upperBound)){upperBound <- pars + 3*abs(pars)}
+
+  # sample points between lower and upper bounds
+  bounds <- cbind(lowerBound, upperBound)
+  samplePoints <- apply(bounds, 1,
+                        function(x) runif(n = nSample,
+                                          min = x[1],
+                                          max = x[2]))
+
+
+  message("Computing fit at sample points: \n")
+  fitAtSamplePoints <- c()
+  pb <- txtProgressBar(min = 0, max = nSample, initial = 0, char = "=",
+                       width = NA, title, label, style = 3, file = "")
+  it <- 1
+  for(samplePoint in 1:nSample){
+    it <- it+1
+    setTxtProgressBar(pb, it)
+    fitAtSamplePoint <- try(getFit(model, pars = samplePoints[samplePoint,]), silent = T)
+    if(any(class(fitAtSamplePoint) == "try-error") || is.na(fitAtSamplePoint)){
+      observedNA <- TRUE
+      fitAtSamplePoints[samplePoint] <- NA
+      next}
+    fitAtSamplePoints[samplePoint] <- fitAtSamplePoint
+  }
+  cat("\n")
+
+  message("Evaluating midpoint convexity for all pairs of sample points: \n")
+  isConvex <- c()
+  elemNames <- c()
+  midpointDifference <- c()
+  observedNA <- FALSE
+  maxChecks <- (sum(!is.na(fitAtSamplePoints))-1)*(sum(!is.na(fitAtSamplePoints)))/2
+  pb <- txtProgressBar(min = 0, max = maxChecks, initial = 0, char = "=",
+                       width = NA, title, label, style = 3, file = "")
+  it <- 1
+  for(samplePoint1 in 1:nSample){
+    f_1 <- fitAtSamplePoints[samplePoint1]
+    if(samplePoint1 == nSample){break}
+    if(is.na(f_1)){next}
+
+    for(samplePoint2 in (samplePoint1+1):nSample){
+      it <- it+1
+      setTxtProgressBar(pb, it)
+
+      f_2 <- fitAtSamplePoints[samplePoint2]
+      if(is.na(f_2)){next}
+      f_3 <- try(getFit(model, pars = (samplePoints[samplePoint1,] + samplePoints[samplePoint2,])/2), silent = T)
+      if(any(class(f_3) == "try-error")){
+        observedNA <- TRUE
+        next}
+      if(anyNA(c(f_1,f_2,f_3))){
+        observedNA <- TRUE
+        next}
+      # check midpoint convexity
+      isConvex <- c(isConvex, f_1 + f_2 >= 2*f_3)
+      elemNames <- c(elemNames, paste0(samplePoint1, " vs ", samplePoint2))
+      midpointDifference <- c(midpointDifference, f_1 + f_2 - 2*f_3)
+    }
+  }
+  cat("\n")
+  if(observedNA){warning("Some sample points resulted in errors / NA. Only the results for non-NA evaluations are reported.")}
+  names(isConvex) <- elemNames
+  names(midpointDifference) <- elemNames
+  rownames(samplePoints) <- 1:nSample
+
+  message(paste0("Of in total ", length(isConvex), " non-NA checks ", sum(!isConvex), " resulted in non-convex evaluations at the mid point."))
+  return(list("samplePoints" = samplePoints, "isConvex" = isConvex, "midpointDifference" = midpointDifference))
+}
+
 #' shinyfy
 #'
 #' creates a shiny application from a regCtsemObject
